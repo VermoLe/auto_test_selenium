000c159505bf481a82bf3cf124bc3e4652090b9e
VermoLe <kiril.egorov.2002@bk.ru>
2024-07-03 16:49:20 +0300

    add selenium scripts from stepik course

---
+++ operagx_lesson2-4_step8.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+from selenium.webdriver.support.ui import Select
+# Укажите путь к ChromeDriver
+chromedriver_path = 'F:/Chromedriver123-0-6312-122/chromedriver.exe'
+
+# Укажите путь к исполняемому файлу Opera GX
+opera_executable_path = 'C:/Users/kiril/AppData/Local/Programs/Opera GX/launcher.exe'
+
+# Настройки для Opera GX
+options = Options()
+options.binary_location = opera_executable_path
+
+options.add_argument('--side-profile-name=33363838345F31383136323831353735')
+options.add_argument('--side-profile-no-gx-sounds')
+options.add_argument('--side-profile-muted')
+options.add_argument('--with-feature:side-profiles')
+options.add_argument('--incognito')
+# Добавьте аргументы командной строки
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
+#options.add_argument('--disable-gpu')
+options.add_argument('--disable-usage-statistics-question')
+options.add_argument('--no-default-browser-check')
+
+
+
+# Создайте сервис для ChromeDriver
+service = Service(chromedriver_path)
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    
+    browser = webdriver.Chrome(options=options, service=service)
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser.get(link)
+    #button =  browser.find_element(By.ID, "price")
+    #price = button.text
+
+    button = WebDriverWait(browser, 10).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    if(button ==True):
+        book =  browser.find_element(By.ID, "book")
+        book.click()
+    
+
+    button2 = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "solve"))
+    )
+    x_element = browser.find_element(By.ID, "input_value")
+
+    x = x_element.text
+    y = calc(x)
+    input_field = browser.find_element(By.CSS_SELECTOR, '#answer')
+    input_field.send_keys(y)
+    button2.click()
+    #message = browser.find_element(By.ID, "verify_message")
+
+    #assert "successful" in message.text
+
+except Exception as error:
+    print(f'Произошла ошибка, вот её трэйсбэк: {error}')
+finally:
+    
+    browser.quit()
+
+#button = WebDriverWait(browser, 5).until(
+#        EC.element_to_be_clickable((By.ID, "verify"))
+#    )
